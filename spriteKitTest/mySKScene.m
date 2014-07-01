a6ff482245c8b77f656792e08a0a00561022838a
YoshinoriMurata <yoshinori@murata-no-MacBook-Air.local>
2014-07-01 15:58:10 +0900

    Xcode test
    test

--- spriteKitTest/mySKScene.m
+++ spriteKitTest/mySKScene.m
@@ -8,18 +8,24 @@
 
 #import "mySKScene.h"
 
+@interface mySKScene(){
+    //調べるラベルを用意する
+    SKLabelNode *mySKLabel;
+}
+@end
+
 @implementation mySKScene
 
 -(instancetype)initWithSize:(CGSize)size{
     if (self = [super initWithSize:size]) {
         //背景に色を指定
         self.backgroundColor = [UIColor blackColor];
-<<<<<<< HEAD
+        
         //スプライトを使って追加する
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"tanikaze"];
-        sprite.position = CGPointMake(160, 200);
-        sprite.size = CGSizeMake(300, 230);
-        [self addChild:sprite];
+       // SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"tanikaze"];
+        //sprite.position = CGPointMake(160, 200);
+        //sprite.size = CGSizeMake(300, 230);
+        //[self addChild:sprite];
         //移動する
         //SKAction *action1 = [SKAction moveTo:CGPointMake(160, 300) duration:2];
         //回転する
@@ -30,20 +36,33 @@
         //SKAction *action = [SKAction rotateByAngle:M_PI_2*2 duration:1];
         //SKAction *actionForever = [SKAction repeatActionForever:action];
         //左下に移動、右上に移動を繰り返す
-        SKAction *move1 = [SKAction moveTo:CGPointMake(280, 200) duration:1];
-        SKAction *move2 = [SKAction moveTo:CGPointMake(50, 400) duration:1];
-        SKAction *goback = [SKAction sequence:@[move1, move2]];
-        [sprite runAction:[SKAction repeatActionForever:goback]];
+        //SKAction *move1 = [SKAction moveTo:CGPointMake(280, 200) duration:1];
+        //SKAction *move2 = [SKAction moveTo:CGPointMake(50, 400) duration:1];
+        //SKAction *goback = [SKAction sequence:@[move1, move2]];
+        //[sprite runAction:[SKAction repeatActionForever:goback]];
         
-=======
+
         //ラベルを追加する
-        SKLabelNode *mySKLabel = [SKLabelNode labelNodeWithFontNamed:@"Marker Felt"];
+        //SKLabelNode *mySKLabel = [SKLabelNode labelNodeWithFontNamed:@"Marker Felt"];
+        mySKLabel = [SKLabelNode labelNodeWithFontNamed:@"Marker Felt"];
         mySKLabel.text = @"Hello World";
         mySKLabel.position = CGPointMake(160, 300);
         [self addChild:mySKLabel];
->>>>>>> FETCH_HEAD
+
     }
     return self;
 }
+
+//タッチされた時、ラベルが押されたか調べる
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    //タッチした座標を取得して、
+    UITouch *touch = [touches anyObject];
+    CGPoint location = [touch locationInNode:self];
+    //ラベルがタッチされたのかどうか調べる
+    if ([mySKLabel containsPoint:location]) {
+        NSLog(@"タッチした");
+    }
+}
+
 
 @end
